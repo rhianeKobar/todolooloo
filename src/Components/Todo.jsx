022f1aedff44a9b3b4ac11638f1db5c96a892957
rhianeKobar <shakira.r.salazar@gmail.com>
2022-06-16 12:20:44 +0100

    add styling and complete functionality

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+import {RiCloseCircleLine} from 'react-icons/ri'
+import {TiEdit} from 'react-icons/ti'
+
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
+
+	const [edit, setEdit] = useState({
+		id: null,
+		value: ''
+	})
+
+	const submitUpdate = value => {
+		updateTodo(edit.id, value)
+		setEdit({
+			id:null,
+			value:''
+		})
+	}
+
+	if(edit.id){
+		return <TodoForm edit={edit} onSubmit={submitUpdate} />
+	}
+
+	return todos.map((todo, index) => (
+		<div className="d-flex justify-content-center">
+			<div 
+				className='d-flex justify-content-center border border-primary m-2 p-2 w-75'
+				key={index}
+			>
+				<div
+					key={todo.id}
+					onClick={() => completeTodo(todo.id)}
+					className='text-primary col-md-10'
+				>
+					{todo.text}
+				</div>
+				<div className='text-primary col-md-2 align-self-center text-center'>
+					<RiCloseCircleLine 
+						onClick={() => removeTodo(todo.id)}
+						className='me-1'
+					/>
+					<TiEdit 
+						onClick={() => setEdit({id: todo.id, value: todo.text})}
+						className='ms-3'
+					/>
+				</div>
+			</div>
+		</div>
+		
+	));
+}
+
+export default Todo
---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useRef }  from 'react';
+import {BsPlusCircleFill} from 'react-icons/bs';
+
+function TodoForm(props) {
+	const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+	const inputRef = useRef(null)
+
+	useEffect(()=>{
+		inputRef.current.focus()
+	})
+
+	const handleChange = e => {
+		setInput(e.target.value);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		props.onSubmit({
+			id: Math.floor(Math.random() * 10000),
+			text: input
+		});
+
+		setInput('');
+	};
+
+	return (
+		<form className='' onSubmit={handleSubmit}>
+			{props.edit ? 
+			(<>
+			<div className='d-flex justify-content-center pb-5'>
+				<div className='input-group w-50'>
+					<input 
+						type="text" 
+						placeholder='Update your task' 
+						value={input}
+						name="text"
+						onChange={handleChange}
+						ref={inputRef}
+						className='form-control'
+					/>
+					<div className='btn btn-primary'>
+							<BsPlusCircleFill 
+							onClick={handleSubmit} 
+							color='#000'
+							fontSize="1.2em"
+							/>
+					</div>
+				</div>
+			</div>
+			</>) : 
+			(<>
+			<div className='d-flex justify-content-center pb-3'>
+				<div className='input-group w-50'>
+					<input 
+						type="text" 
+						placeholder='Add a task' 
+						value={input}
+						name="text"
+						onChange={handleChange}
+						ref={inputRef}
+						className='form-control'
+					/>
+					<div className='btn btn-primary'>
+							<BsPlusCircleFill 
+							onClick={handleSubmit} 
+							color='#000'
+							fontSize="1.2em"
+							/>
+					</div>
+				</div>
+			</div>
+			</>)
+			}
+			
+			
+		</form>
+	)
+}
+
+export default TodoForm
